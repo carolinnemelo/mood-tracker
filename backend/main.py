d3db8cc550118e6eea3f8161d985b7d1b705a0c0
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-04-30 16:25:01 +0200

    Add initial implementation of mood tracking API with FastAPI

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class MoodEntry(BaseModel):
+    emoji: str
+    note: str | None = None
+    
+class MoodDB: 
+  def __init__(self):
+    self.data = []
+    
+  def add_mood(self, mood: dict):
+    self.data.append(mood)
+    
+  def get_all(self):
+    return self.data
+  
+db = MoodDB()
+
+app = FastAPI()
+ 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Carol"}
+
+
+@app.post("/moods")
+def add_mood(entry: MoodEntry):
+    print("New mood entry received: ", entry)
+    return { "status": "ok", "emoji": entry.emoji, "note": entry.note}
+
+    
+@app.get("/moods")
+def get_moods():
+  return db.get_all()
