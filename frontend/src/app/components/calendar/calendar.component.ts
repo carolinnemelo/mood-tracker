0916ffbde450bc6077f739fe25196d6f2d3a7ef7
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-05-05 14:04:12 +0200

    Refactor calendar component to improve event handling and structure

--- frontend/src/app/components/calendar/calendar.component.ts
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions } from '@fullcalendar/core';
+import {
+  CalendarOptions,
+  EventInput,
+  EventSourceInput,
+} from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';  
-import interactionPlugin from '@fullcalendar/interaction';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 
 
 
@@ -17,10 +21,21 @@ export class CalendarComponent {
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin, interactionPlugin],
-    dateClick: () => this.handleDateClick.bind(this),
+    dateClick: this.handleDateClick.bind(this),
+    events: [],
   };
 
-  handleDateClick(info: string) {
-    console.log('Date clicked: ' + info);
+  clickedDate = '';
+
+  handleDateClick(info: DateClickArg) {
+    this.clickedDate = info.dateStr;
+    const events = this.calendarOptions.events as EventInput[];
+    events.push({
+      title: '😄',
+      date: this.clickedDate,
+    });
+
+    this.calendarOptions = { ...this.calendarOptions, events: [...events] };
+    console.log('events', events);
   }
 }
