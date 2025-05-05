e00364b085c5772a3ef2d87649e4b108d5d5f2cb
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-05-05 16:01:05 +0200

    Refactor calendar component to handle clicked day element and update menu style

--- frontend/src/app/components/calendar/calendar.component.ts
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -1,23 +1,19 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import {
-  CalendarOptions,
-  EventInput,
-  EventSourceInput,
-} from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';  
+import { CalendarOptions, EventInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
-
-
-
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
-  imports: [FullCalendarModule],
+  imports: [FullCalendarModule, NgStyle],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
 })
 export class CalendarComponent {
+  @ViewChild('calendar') calendarRef!: ElementRef;
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin, interactionPlugin],
@@ -26,9 +22,12 @@ export class CalendarComponent {
   };
 
   clickedDate = '';
+  isMenuOpen = false;
+  clickedDayElement: HTMLElement | null = null;
 
   handleDateClick(info: DateClickArg) {
     this.clickedDate = info.dateStr;
+    this.clickedDayElement = info.dayEl;
     const events = this.calendarOptions.events as EventInput[];
     events.push({
       title: '😄',
@@ -36,6 +35,18 @@ export class CalendarComponent {
     });
 
     this.calendarOptions = { ...this.calendarOptions, events: [...events] };
-    console.log('events', events);
+    this.menuStyle;
+  }
+
+  get menuStyle() {
+    if (!this.clickedDayElement) return;
+    const rect = this.clickedDayElement.getBoundingClientRect();
+    return {
+    position: 'absolute',
+    top: `${rect.top + window.scrollY}px`,
+    left: `${rect.left + window.scrollX}px`,
+    width: `${rect.width}px`,
+    height: `${rect.height}px`,
+  };
   }
 }
